dec60c5402a82ced70ebc59bbe0e2497c61d2891
Kashif Minhaj <dailypctips@gmail.com>
2017-07-18 10:55:16 +0530

    Add todo stuff in home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  // todoItems must be fetched from the server!
+  todoItems: any[] = [
+    { title: 'Car service tomorrow at 10.30AM',
+      description: 'Remind Mr. Gautham to get his car for service tomorrow!',
+      actions: ['Call', 'SMS'],
+      time: '10:30AM'
+    },
+    { title: 'Car service tomorrow at 10.30AM',
+      description: 'Remind Mr. Gautham to get his car for service tomorrow!',
+      actions: ['Call', 'SMS'],
+      time: '10:30AM'
+    },
+    { title: 'Car service tomorrow at 10.30AM',
+      description: 'Remind Mr. Gautham to get his car for service tomorrow!',
+      actions: ['Call', 'SMS'],
+      time: '10:30AM'
+    }
+  ]
 
   constructor() { }
 
