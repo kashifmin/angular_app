ce49c9d526e247be793f4aa6c3a3fa0ffd8f5d32
Kashif Minhaj <dailypctips@gmail.com>
2017-07-23 10:08:31 +0530

    Add angular-calendar and sales component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { CalendarModule } from 'angular-calendar';
+
 import { HomeComponent } from './home/home.component';
 import { CustomerComponent } from './customer/customer.component';
+import { SalesComponent } from './sales/sales.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'customer', component: CustomerComponent}
+  { path: 'customer', component: CustomerComponent},
+  { path: 'sales', component: SalesComponent }
 ]
 
 @NgModule({
@@ -20,12 +24,14 @@ const routes: Routes = [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    CustomerComponent
+    CustomerComponent,
+    SalesComponent
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    CalendarModule.forRoot()
   ],
   
   providers: [],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  startOfDay,
+  endOfDay,
+  subDays,
+  addDays,
+  endOfMonth,
+  isSameDay,
+  isSameMonth,
+  addHours
+} from 'date-fns';
+import {
+  CalendarEvent,
+  CalendarEventAction,
+  CalendarEventTimesChangedEvent
+} from 'angular-calendar';
+
+const colors: any = {
+  red: {
+    primary: '#ad2121',
+    secondary: '#FAE3E3'
+  },
+  blue: {
+    primary: '#1e90ff',
+    secondary: '#D1E8FF'
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#FDF1BA'
+  }
+};
 
 @Component({
   selector: 'app-home',
@@ -23,7 +53,39 @@ export class HomeComponent implements OnInit {
       actions: ['Call', 'SMS'],
       time: '10:30AM'
     }
-  ]
+  ];
+
+  viewDate: Date = new Date();
+
+  
+
+  events: CalendarEvent[] = [
+    {
+      start: subDays(startOfDay(new Date()), 1),
+      end: addDays(new Date(), 1),
+      title: 'A 3 day event',
+      color: colors.red,
+
+    },
+    {
+      start: startOfDay(new Date()),
+      title: 'An event with no end date',
+      color: colors.yellow,
+
+    },
+    {
+      start: subDays(endOfMonth(new Date()), 3),
+      end: addDays(endOfMonth(new Date()), 3),
+      title: 'A long event that spans 2 months',
+      color: colors.blue
+    },
+    {
+      start: addHours(startOfDay(new Date()), 2),
+      end: new Date(),
+      title: 'A draggable and resizable event',
+      color: colors.yellow
+    }
+  ];
 
   constructor() { }
 
